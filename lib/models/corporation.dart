d43afbefbe74647f3bf82de6d5ba3de8eedf3e10
heejoong <heejoong@sonssoft.com>
2020-07-27 18:38:15 +0900

    fixed issue-2
    CorpClass 영문 제거

--- lib/models/corporation.dart
+++ lib/models/corporation.dart
@@ -60,13 +60,13 @@ extension CorpClassToString on CorpClass {
   String get enumToString {
     switch (this) {
       case CorpClass.Y:
-        return 'Y 유가';
+        return '유가';
       case CorpClass.K:
-        return 'K 코스닥';
+        return '코스닥';
       case CorpClass.N:
-        return 'N 코넥스';
+        return '코넥스';
       case CorpClass.E:
-        return 'E 기타';
+        return '기타';
       default:
         throw Exception('CorpClass is not defined');
     }
